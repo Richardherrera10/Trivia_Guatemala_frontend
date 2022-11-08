d417a9e33340702618eace528e6683586c85341d
Ricardo Herrera <richard.andre.herrera@gmail.com>
2022-11-08 17:07:57 -0600

    hash router

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './routes/App'
-
+import { HashRouter } from 'react-router-dom'
 import {BrowserRouter} from 'react-router-dom'
 import { UserProvider} from './UserContext'
 ReactDOM.createRoot(document.getElementById('root')).render(
   
  
  
-      <BrowserRouter>
+      <HashRouter>
        
             <App />
       
-      </BrowserRouter>
+      </HashRouter>
    
 
     
